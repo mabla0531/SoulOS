48021a8d1e5ad92038dc695d3bb0b6c81636a7e0
Matthew Bland <MEBland8141@muleriders.saumag.edu>
2021-12-17 00:47:35 -0600

    Completed VGA Buffer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #![no_std]
+#![no_main]
+
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
-fn main() {
-    
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello!");
+
+    loop {}
 }
